ab2c56c658a4d4fc29c06dddd2f35676511b5633
FERDdeveloper <86313430+ferderplays@users.noreply.github.com>
2022-10-18 23:04:15 +0200

    Rewrite

---
+++ src/LimoBrowserApp/InnitNewTab.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace LimoBrowserApp
+{
+    /// <summary>
+    /// Interakční logika pro InnitNewTab.xaml
+    /// </summary>
+    public partial class InnitNewTab : UserControl
+    {
+        public InnitNewTab()
+        {
+            InitializeComponent();
+            SetupElemsFromXAML();
+        }
+
+        public void SetupElemsFromXAML()
+        {
+            navbar.Width = SystemParameters.PrimaryScreenWidth;
+            navbar.Height = 30;
+            www.Height = (SystemParameters.PrimaryScreenHeight - 30) - 105;
+            def1.Width = new GridLength((SystemParameters.PrimaryScreenWidth - 85) - 65);
+            urlbar.Width = (SystemParameters.PrimaryScreenWidth - 85) - 65;
+        }
+    }
+}
---
+++ src/LimoBrowserApp/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace LimoBrowserApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        public void AddTabToTabControl(object sender, RoutedEventArgs e)
+        {
+            TabItem newtab = new TabItem();
+            InnitNewTab tab = new InnitNewTab();
+            newtab.Content = tab;
+            newtab.Style = (Style)FindResource("tabStyle");
+            tabs.Items.Add(newtab);
+        }
+    }
+}
