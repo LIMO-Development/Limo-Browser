a250e660e1f2be0c1ea955cc55488f6a16c0b16f
FERDdeveloper <86313430+ferderplays@users.noreply.github.com>
2022-10-22 23:38:47 +0200

    v1.1.0

--- src/LimoBrowserApp/InnitNewTab.xaml.cs
+++ src/LimoBrowserApp/InnitNewTab.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -24,6 +25,7 @@ namespace LimoBrowserApp
         {
             InitializeComponent();
             SetupElemsFromXAML();
+            ParseThemeFile();
         }
 
         public void SetupElemsFromXAML()
@@ -33,7 +35,7 @@ namespace LimoBrowserApp
             def2.Width = new GridLength(SystemParameters.PrimaryScreenWidth - 30);
             www.Height = (SystemParameters.PrimaryScreenHeight - 60) - 105;
             def1.Width = new GridLength(SystemParameters.PrimaryScreenWidth - 130);
-            urlbar.Width = SystemParameters.PrimaryScreenWidth - 90;
+            urlbar.Width = SystemParameters.PrimaryScreenWidth - 140;
         }
 
         public void LoadCefSharpWebPage(object sender, RoutedEventArgs e)
@@ -66,20 +68,187 @@ namespace LimoBrowserApp
                 case false:
                     // columns
                     ColumnDefinition settingsdef = new ColumnDefinition();
-                    settingsdef.Width = new GridLength(100);
+                    settingsdef.Width = new GridLength(300);
                     browserGrid.ColumnDefinitions.Add(settingsdef);
 
                     // initializer
-                    StackPanel settingspanel = new StackPanel();
-                    settingspanel.Background = Brushes.DarkRed;
+                    Grid settingspanel = new Grid();
+                    settingspanel.Background = Brushes.DimGray;
+
+                    // style
+                    RowDefinition rowdef1 = new RowDefinition();
+                    rowdef1.Height = new GridLength(50);
+
+                    Grid setting1 = new Grid();
+                    Grid.SetRow(setting1, 0);
+
+                    ColumnDefinition columndef1 = new ColumnDefinition();
+                    columndef1.Width = new GridLength(150);
+                    ColumnDefinition columndef2 = new ColumnDefinition();
+                    columndef2.Width = new GridLength(150);
+
+                    TextBlock text1 = new TextBlock();
+                    text1.Text = "Theme: ";
+                    Grid.SetColumn(text1, 0);
+
+                    ScrollViewer themesviewer = new ScrollViewer();
+                    themesviewer.VerticalScrollBarVisibility = ScrollBarVisibility.Disabled;
+                    Grid.SetColumn(themesviewer, 1);
+
+                    setting1.Children.Add(text1);
+                    setting1.Children.Add(themesviewer);
+
+                    setting1.ColumnDefinitions.Add(columndef1);
+                    setting1.ColumnDefinitions.Add(columndef2);
 
                     // adder
+                    settingspanel.Children.Add(setting1);
+                    settingspanel.RowDefinitions.Add(rowdef1);
                     browserGrid.Children.Add(settingspanel);
 
                     // setter
                     Grid.SetColumn(settingspanel, 2);
-                    def2.Width = new GridLength(SystemParameters.PrimaryScreenWidth - 130);
+                    def2.Width = new GridLength(SystemParameters.PrimaryScreenWidth - 330);
                     settingsShown = true;
+                    break;
+            }
+        }
+
+        public void ParseThemeFile()
+        {
+            string file = "C:\\Users\\" + Environment.UserName + "\\AppData\\Local\\LIMO-Development\\LIMO-Browser\\settings\\Themes.limobrowsertheme";
+            switch (System.IO.File.Exists(file))
+            {
+                case true:
+                    foreach (string line in System.IO.File.ReadAllLines(file))
+                    {
+                        switch (line)
+                        {
+                            case "theme:red;":
+                                navbar.Background = Brushes.DarkRed;
+                                urlbar.Background = Brushes.Red;
+                                urlbar.BorderBrush = Brushes.Red;
+                                sidebar.Background = Brushes.DarkRed;
+                                goButton.Background = Brushes.Red;
+                                goButton.BorderBrush = Brushes.Red;
+                                settingsButton.Background = Brushes.Red;
+                                settingsButton.BorderBrush = Brushes.Red;
+                                break;
+
+                            case "theme:orange;":
+                                navbar.Background = Brushes.DarkOrange;
+                                urlbar.Background = Brushes.Orange;
+                                urlbar.BorderBrush = Brushes.Orange;
+                                sidebar.Background = Brushes.DarkOrange;
+                                goButton.Background = Brushes.Orange;
+                                goButton.BorderBrush = Brushes.Orange;
+                                settingsButton.Background = Brushes.Orange;
+                                settingsButton.BorderBrush = Brushes.Orange;
+                                break;
+
+                            case "theme:yellow;":
+                                navbar.Background = Brushes.Gold;
+                                urlbar.Background = Brushes.Yellow;
+                                urlbar.BorderBrush = Brushes.Yellow;
+                                sidebar.Background = Brushes.Gold;
+                                goButton.Background = Brushes.Yellow;
+                                goButton.BorderBrush = Brushes.Yellow;
+                                settingsButton.Background = Brushes.Yellow;
+                                settingsButton.BorderBrush = Brushes.Yellow;
+                                break;
+
+                            case "theme:yellowgreen;":
+                                navbar.Background = Brushes.YellowGreen;
+                                urlbar.Background = Brushes.GreenYellow;
+                                urlbar.BorderBrush = Brushes.GreenYellow;
+                                sidebar.Background = Brushes.YellowGreen;
+                                goButton.Background = Brushes.GreenYellow;
+                                goButton.BorderBrush = Brushes.GreenYellow;
+                                settingsButton.Background = Brushes.GreenYellow;
+                                settingsButton.BorderBrush = Brushes.GreenYellow;
+                                break;
+
+                            case "theme:green;":
+                                navbar.Background = Brushes.DarkGreen;
+                                urlbar.Background = Brushes.Lime;
+                                urlbar.BorderBrush = Brushes.Lime;
+                                sidebar.Background = Brushes.DarkGreen;
+                                goButton.Background = Brushes.Lime;
+                                goButton.BorderBrush = Brushes.Lime;
+                                settingsButton.Background = Brushes.Lime;
+                                settingsButton.BorderBrush = Brushes.Lime;
+                                break;
+
+                            case "theme:aquamarine;":
+                                navbar.Background = Brushes.CadetBlue;
+                                urlbar.Background = Brushes.Aquamarine;
+                                urlbar.BorderBrush = Brushes.Aquamarine;
+                                sidebar.Background = Brushes.CadetBlue;
+                                goButton.Background = Brushes.Aquamarine;
+                                goButton.BorderBrush = Brushes.Aquamarine;
+                                settingsButton.Background = Brushes.Aquamarine;
+                                settingsButton.BorderBrush = Brushes.Aquamarine;
+                                break;
+
+                            case "theme:cyan;":
+                                navbar.Background = Brushes.DarkCyan;
+                                urlbar.Background = Brushes.Cyan;
+                                urlbar.BorderBrush = Brushes.Cyan;
+                                sidebar.Background = Brushes.DarkCyan;
+                                goButton.Background = Brushes.Cyan;
+                                goButton.BorderBrush = Brushes.Cyan;
+                                settingsButton.Background = Brushes.Cyan;
+                                settingsButton.BorderBrush = Brushes.Cyan;
+                                break;
+
+                            case "theme:lightblue;":
+                                navbar.Background = Brushes.LightSkyBlue;
+                                urlbar.Background = Brushes.LightBlue;
+                                urlbar.BorderBrush = Brushes.LightBlue;
+                                sidebar.Background = Brushes.LightSkyBlue;
+                                goButton.Background = Brushes.LightBlue;
+                                goButton.BorderBrush = Brushes.LightBlue;
+                                settingsButton.Background = Brushes.LightBlue;
+                                settingsButton.BorderBrush = Brushes.LightBlue;
+                                break;
+
+                            case "theme:blue;":
+                                navbar.Background = Brushes.DarkBlue;
+                                urlbar.Background = Brushes.Blue;
+                                urlbar.BorderBrush = Brushes.Blue;
+                                sidebar.Background = Brushes.DarkBlue;
+                                goButton.Background = Brushes.Blue;
+                                goButton.BorderBrush = Brushes.Blue;
+                                settingsButton.Background = Brushes.Blue;
+                                settingsButton.BorderBrush = Brushes.Blue;
+                                break;
+
+                            case "theme:purple;":
+                                navbar.Background = Brushes.Purple;
+                                urlbar.Background = Brushes.DarkOrchid;
+                                urlbar.BorderBrush = Brushes.DarkOrchid;
+                                sidebar.Background = Brushes.Purple;
+                                goButton.Background = Brushes.DarkOrchid;
+                                goButton.BorderBrush = Brushes.DarkOrchid;
+                                settingsButton.Background = Brushes.DarkOrchid;
+                                settingsButton.BorderBrush = Brushes.DarkOrchid;
+                                break;
+
+                            case "theme:pink;":
+                                navbar.Background = Brushes.Plum;
+                                urlbar.Background = Brushes.Pink;
+                                urlbar.BorderBrush = Brushes.Pink;
+                                sidebar.Background = Brushes.Plum;
+                                goButton.Background = Brushes.Pink;
+                                goButton.BorderBrush = Brushes.Pink;
+                                settingsButton.Background = Brushes.Pink;
+                                settingsButton.BorderBrush = Brushes.Pink;
+                                break;
+                        }
+                    }
+                    break;
+
+                case false:
                     break;
             }
         }
--- src/LimoBrowserApp/MainWindow.xaml.cs
+++ src/LimoBrowserApp/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,6 +24,7 @@ namespace LimoBrowserApp
         public MainWindow()
         {
             InitializeComponent();
+            LoadFiles();
         }
 
         public void AddTabToTabControl(object sender, RoutedEventArgs e)
@@ -39,6 +41,117 @@ namespace LimoBrowserApp
         {
             ((Button)sender).Background = Brushes.Transparent;
             ((Button)sender).BorderBrush = Brushes.Transparent;
+        }
+
+        public void LoadFiles()
+        {
+            string name = "C:\\Users\\" + Environment.UserName + "\\AppData\\Local\\LIMO-Development";
+            switch (Directory.Exists(name))
+            {
+                case true:
+                    string name2 = name + "\\LIMO-Browser";
+                    switch (Directory.Exists(name2))
+                    {
+                        case true:
+                            string name3 = name2 + "\\settings";
+                            switch (Directory.Exists(name3))
+                            {
+                                case true:
+                                    string name4 = name3 + "\\Themes.limobrowsertheme";
+                                    switch (File.Exists(name4))
+                                    {
+                                        case true:
+                                            ParseThemeFile();
+                                            break;
+
+                                        case false:
+                                            File.Create(name4);
+                                            break;
+                                    }
+                                    break;
+
+                                case false:
+                                    Directory.CreateDirectory(name3);
+                                    break;
+                            }
+                            break;
+
+                        case false:
+                            Directory.CreateDirectory(name2);
+                            break;
+                    }
+                    break;
+
+                case false:
+                    Directory.CreateDirectory(name);
+                    break;
+            }
+        }
+
+        public void ParseThemeFile()
+        {
+            string file = "C:\\Users\\" + Environment.UserName + "\\AppData\\Local\\LIMO-Development\\LIMO-Browser\\settings\\Themes.limobrowsertheme";
+            foreach (string line in File.ReadAllLines(file))
+            {
+                switch (line)
+                {
+                    case "theme:red;":
+                        allControls.Background = Brushes.DarkRed;
+                        tabs.Background = Brushes.DarkRed;
+                        break;
+
+                    case "theme:orange;":
+                        allControls.Background = Brushes.DarkOrange;
+                        tabs.Background = Brushes.DarkOrange;
+                        break;
+
+                    case "theme:yellow;":
+                        allControls.Background = Brushes.Gold;
+                        tabs.Background = Brushes.Gold;
+                        break;
+
+                    case "theme:yellowgreen;":
+                        allControls.Background = Brushes.YellowGreen;
+                        tabs.Background = Brushes.YellowGreen;
+                        break;
+
+                    case "theme:green;":
+                        allControls.Background = Brushes.DarkGreen;
+                        tabs.Background = Brushes.DarkGreen;
+                        break;
+
+
+                    case "theme:aquamarine;":
+                        allControls.Background = Brushes.CadetBlue;
+                        tabs.Background = Brushes.CadetBlue;
+                        break;
+
+                    case "theme:cyan;":
+                        allControls.Background = Brushes.DarkCyan;
+                        tabs.Background = Brushes.DarkCyan;
+                        break;
+
+                    case "theme:lightblue;":
+                        allControls.Background = Brushes.LightSkyBlue;
+                        tabs.Background = Brushes.LightSkyBlue;
+                        break;
+
+                    case "theme:blue;":
+                        allControls.Background = Brushes.DarkBlue;
+                        tabs.Background = Brushes.DarkBlue;
+                        break;
+
+                    case "theme:purple;":
+                        allControls.Background = Brushes.Purple;
+                        tabs.Background = Brushes.Purple;
+                        break;
+
+                    case "theme:pink;":
+                        allControls.Background = Brushes.Plum;
+                        tabs.Background = Brushes.Plum;
+                        break;
+                }
+            }
         }
     }
 }
